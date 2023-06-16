2fb1d3f7c85651e92e0854e18e6f53dd5c1f1f6e
Stephen Nguyen <111164465+tungbinhdinh89@users.noreply.github.com>
2023-06-16 18:00:26 +0700

    set up react router dom

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import Container from "react-bootstrap/esm/Container";
 import "./App.scss";
-import Header from "./components/Header";
 import TableUsers from "./components/TableUsers";
+import Home from "./components/Home";
+import { Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+import UserTemplate from "./components";
 
 function App() {
   return (
     <>
       <div className="app-container">
-        <Header />
-        <Container>
-          <TableUsers />
-        </Container>
+        <Routes>
+          <Route path="/" element={<UserTemplate Component={Home} />} />
+          <Route
+            path="/users"
+            element={<UserTemplate Component={TableUsers} />}
+          />
+        </Routes>
       </div>
       <ToastContainer
         position="top-right"
