0aaea54eb0c4348b82f886bc2e1da1d262c36f73
Stephen Nguyen <111164465+tungbinhdinh89@users.noreply.github.com>
2023-06-16 22:46:24 +0700

    edit login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,28 +1,49 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { loginApi } from "../services/UserService";
 import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [isShowPassword, setIsShowPassword] = useState(false);
+  const [loadingAPI, setLoadingAPI] = useState(false);
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    let token = localStorage.getItem("token");
+    if (token) {
+      navigate("/");
+    }
+  }, []);
 
   const handleLogin = async () => {
     if (!email || !password) {
       toast.error("Email/Password is required!");
       return;
+    }
+    setLoadingAPI(true);
+    const res = await loginApi(email, password);
+    console.log("check res >>> :", res);
+    if (res && res.token) {
+      // success
+      localStorage.setItem("token", res.token);
+      toast.success("Login Success!");
+      navigate("/");
     } else {
-      const res = await loginApi("eve.holt@reqres.in", password);
-      if (res && res.token) {
-        localStorage.setItem("token", res.token);
+      // error
+      if (res && res.status === 400) {
+        toast.error(res.data.error);
       }
     }
+    setLoadingAPI(false);
   };
   return (
     <>
       <div className="login-container col-12 col-sm-4">
         <div className="title">Login</div>
-        <div className="text">Email or username</div>
+        <div className="text">Email or username (eve.holt@reqres.in)</div>
         <input
           type="text"
           placeholder="Email or username..."
@@ -50,7 +71,7 @@ const Login = () => {
           className={email && password ? "btn btn-primary" : ""}
           disabled={email && password ? false : true}
           onClick={() => handleLogin()}>
-          Login
+          {loadingAPI && <i className="fa-solid fa-sync fa-spin" />} Login
         </button>
         <div className="back">
           <i className="fa-solid fa-angles-left"></i>Go back
