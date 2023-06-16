a1c9122f314511d9613f7f95c263578c8501dbda
Stephen Nguyen <111164465+tungbinhdinh89@users.noreply.github.com>
2023-06-16 18:01:07 +0700

    edit layout header, add NavLink component

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,30 +2,41 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import logoApp from "../assets/pngegg.png";
+import { useLocation, NavLink } from "react-router-dom";
 
 const Header = (props) => {
+  const location = useLocation();
   return (
     <>
       <Navbar bg="light" expand="lg">
         <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Brand href="#home">
+            <img
+              src={logoApp}
+              width="30"
+              height="30"
+              className="d-inline-block align-top mx-2"
+              alt="logoApp"
+            />
+            <span>QLNV's App</span>
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
+            <Nav className="me-auto" activeKey={location.pathname}>
+              <NavLink to="/" className="nav-link">
+                Home
+              </NavLink>
+
+              <NavLink to="/users" className="nav-link">
+                Manage Users
+              </NavLink>
+            </Nav>
+
+            <Nav>
+              <NavDropdown title="Setting" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Login</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Logout</NavDropdown.Item>
               </NavDropdown>
             </Nav>
           </Navbar.Collapse>
