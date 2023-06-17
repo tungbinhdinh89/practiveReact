b068be55e5a5c87693dfbeba92836f3db156755c
Stephen Nguyen <111164465+tungbinhdinh89@users.noreply.github.com>
2023-06-17 10:14:48 +0700

    edit login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,22 +1,18 @@
 import { useEffect, useState } from "react";
 import { loginApi } from "../services/UserService";
 import { toast } from "react-toastify";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate, NavLink } from "react-router-dom";
+import { useContext } from "react";
+import { UserContext } from "../context/UserContext";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [isShowPassword, setIsShowPassword] = useState(false);
   const [loadingAPI, setLoadingAPI] = useState(false);
+  const { loginContext } = useContext(UserContext);
 
   const navigate = useNavigate();
-
-  useEffect(() => {
-    let token = localStorage.getItem("token");
-    if (token) {
-      navigate("/");
-    }
-  }, []);
 
   const handleLogin = async () => {
     if (!email || !password) {
@@ -28,7 +24,7 @@ const Login = () => {
     console.log("check res >>> :", res);
     if (res && res.token) {
       // success
-      localStorage.setItem("token", res.token);
+      loginContext(email, res.token);
       toast.success("Login Success!");
       navigate("/");
     } else {
@@ -73,9 +69,11 @@ const Login = () => {
           onClick={() => handleLogin()}>
           {loadingAPI && <i className="fa-solid fa-sync fa-spin" />} Login
         </button>
-        <div className="back">
-          <i className="fa-solid fa-angles-left"></i>Go back
-        </div>
+        <Link to="/" className="nav-link text-black">
+          <div className="back">
+            <i className="fa-solid fa-angles-left"></i>Go back
+          </div>
+        </Link>
       </div>
     </>
   );
