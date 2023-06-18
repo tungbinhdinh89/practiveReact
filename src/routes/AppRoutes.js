19dbeb2743a8d3df5cf0ba049d4fc8f0503d112e
Stephen Nguyen <111164465+tungbinhdinh89@users.noreply.github.com>
2023-06-18 12:08:09 +0700

    add notfound route

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -4,6 +4,7 @@ import TableUsers from "../components/TableUsers";
 import Home from "../components/Home";
 import Login from "../components/Login";
 import PrivateRoute from "./PrivateRoute";
+import NotFound from "../components/NotFound";
 
 const AppRoutes = () => {
   return (
@@ -11,6 +12,8 @@ const AppRoutes = () => {
       <Routes>
         <Route path="/" element={<UserTemplate Component={Home} />} />
         <Route path="/login" element={<UserTemplate Component={Login} />} />
+        <Route path="*" element={<UserTemplate Component={NotFound} />} />
+
         <Route
           path="/users"
           element={
